c27ed794a8e766081016abf36936ee268a5bc43c
Trey Rumsey <treyrumz@gmail.com>
2023-03-27 11:22:07 -0400

    Add vite-plugin-checker to dev dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import Checker from "vite-plugin-checker";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
@@ -6,5 +7,11 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), eslint()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+    eslint(),
+    Checker({ typescript: true, overlay: true }),
+  ],
 });
